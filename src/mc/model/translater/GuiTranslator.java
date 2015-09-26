01a468dd0bdcd49b39a2ca61396fdc1ba320e664
anujkaushik <anujkaushik3107@gmail.com>
2015-09-26 18:38:21 +0530

    deleting old (before maven) files

--- src/mc/model/translater/GuiTranslator.java
+++
@@ -1,9 +0,0 @@
-package mc.model.translater;
-
-import mc.event.a2g.RootEventA2G.EventTypeA2G;
-
-public interface GuiTranslator {
-	
-	public Object getGuiDisplayObject(EventTypeA2G eventTypeA2G, Object modelResponse);
-	
-}
