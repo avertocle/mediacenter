b464e92995901f01f82c88a9cd4393dd9ec9f56e
anujkaushik3107 <anujkaushik3107@gmail.com>
2015-10-22 07:54:59 +0530

    password field in password input dialog

--- src/main/java/mc/gui/LoginHandler.java
+++ src/main/java/mc/gui/LoginHandler.java
@@ -1,24 +1,34 @@
 package mc.gui;
 
 import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
 
 import mc.auth.AuthenticationHandler;
 
 public class LoginHandler {
-	
+
 	private AuthenticationHandler authenticationHandler;
-	
+
 	public LoginHandler(AuthenticationHandler authenticationHandler) {
 		this.authenticationHandler = authenticationHandler;
 	}
-	
-	public boolean doLogin(String username){
-		
-		String pass = JOptionPane.showInputDialog(null, "Enter Password for " + username, "Login", JOptionPane.QUESTION_MESSAGE);
-		boolean authResult = authenticationHandler.authenticate(username, pass);
-		if(!authResult){
-			JOptionPane.showMessageDialog(null, "Login Failure", "Login", JOptionPane.ERROR_MESSAGE);
+
+	public boolean doLogin(String username) {
+
+		String msg = "Enter Password for " + username;
+		JPasswordField pf = new JPasswordField();
+		int choice = JOptionPane.showConfirmDialog(null, pf, msg, JOptionPane.OK_CANCEL_OPTION,
+				JOptionPane.PLAIN_MESSAGE);
+
+		boolean authResult = false;
+		if (choice == JOptionPane.OK_OPTION) {
+			String pass = new String(pf.getPassword());
+			authResult = authenticationHandler.authenticate(username, pass);
+			if (!authResult) {
+				JOptionPane.showMessageDialog(null, "Login Failure", "Login", JOptionPane.ERROR_MESSAGE);
+			}
 		}
+
 		return authResult;
 	}
 
